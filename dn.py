020d060eb1569f8da40dab54ceff04dec2784ca5
Sebastian Danilsons <alzeht@gmail.com>
2021-02-01 11:10:01 +0100

    Converted notebook into python script

---
+++ dn.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+
+def main():
+    dn_response = requests.get("https://www.dn.se/")
+    dn_html = BeautifulSoup(dn_response.text, "html.parser")
+
+    teaser_container = dn_html.find_all('div', {'class': 'teaser-package__content'})
+
+    master_list = []
+
+    for teaser in teaser_container:
+        data_dict = {}
+        tc = teaser.find("div", {"class": "teaser__content"})
+
+        try:
+            title = ' '.join(tc.h1.text.split())
+            title = title.replace('•', '').strip()
+            data_dict['Title'] = title
+        except:
+            continue
+        
+        try:
+            subtitle = ' '.join(tc.p.text.split())
+            subtitle = subtitle.replace('•', '-').replace('TV | TEXT', '').replace('LIVE', '').strip()
+            data_dict['Subtitle'] = subtitle
+        except:
+            data_dict['Subtitle'] = ''
+        
+        try:
+            link = 'https://www.dn.se'+teaser.find('a')['href']
+            data_dict['Link'] = link
+        except:
+            pass
+        master_list.append(data_dict)
+
+    df = pd.DataFrame(master_list)
+
+    from datetime import datetime
+    date = datetime.today().strftime('%Y-%m-%d_%H:%M:%S')
+    df.to_csv(f'DN_Förstasidan_{date}.csv', index=False)
+
+if __name__ == "__main__":
+    main()
